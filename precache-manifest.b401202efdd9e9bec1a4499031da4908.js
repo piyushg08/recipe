4dca5ef536e744c82e9c558933f93d256ab3af76
piyush <piyushg296@gmail.com>
2020-08-27 18:13:50 +0530

    Updates

---
+++ precache-manifest.b401202efdd9e9bec1a4499031da4908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11221c7aebac31b000c0bef076243b3",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "9910ae7207cb64a9c9ef",
+    "url": "/recipe/static/css/main.70dc0702.chunk.css"
+  },
+  {
+    "revision": "980a5d982c89e88bd151",
+    "url": "/recipe/static/js/2.2909ccaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe/static/js/2.2909ccaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9910ae7207cb64a9c9ef",
+    "url": "/recipe/static/js/main.50624e55.chunk.js"
+  },
+  {
+    "revision": "c148dfbfd7f60ce19590",
+    "url": "/recipe/static/js/runtime-main.32223898.js"
+  }
+]);
